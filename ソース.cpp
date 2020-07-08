68b577924c275c9517b657a2d95c7aa9e0fc4949
hj-g18002 <hj-g18002@sist.ac.jp>
2020-07-08 13:16:38 +0900

    必要そうなマクロの定義

--- ソース.cpp
+++ ソース.cpp
@@ -1,4 +1,5 @@
 #include "DxLib.h"
+#include <math.h>
 
 //########## マクロ定義 ##########
 #define GAME_WIDTH			1000	//画面の横の大きさ
@@ -6,7 +7,115 @@
 #define GAME_COLOR			32	//画面のカラービット
 
 #define GAME_WINDOW_BAR		0				//タイトルバーはデフォルトにする
-#define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
+#define GAME_WINDOW_NAME	"I am MAGURO"	//ウィンドウのタイトル
+
+#define GAME_FPS			60		//FPS
+
+//パスの長さ  255文字まで
+#define PATH_MAX	255
+#define NAME_MAX	255
+
+//3Dモデルのパス
+#define MODEL_MAGURO_PATH	TEXT(".\\mqoモデル\\マグロ\\マグロ.mqo")
+#define MODEL_EBI_PATH		TEXT(".\\mqoモデル\\エビ\\エビ.mqo")
+#define MODEL_IKA_PATH		TEXT(".\\mqoモデル\\イカ\\イカ.mqo")
+#define MODEL_PURA_PATH		TEXT(".\\mqoモデル\\プランクトン\\プランクトン.mqo")
+
+//画像のパス(仮置き)
+#define IMAGE_TITLE_BK_PATH		//タイトルの背景画像
+#define IMAGE_TITLE_LOGO_PATH	//タイトルロゴ
+#define IMAGE_TITLE_START_PATH	//はじめる
+#define IMAGE_TITLE_END_PATH	//おわる
+#define IMAGE_TITLE_HOWTO_PATH	//あそびかた
+
+//カメラの設定
+#define CAMERA_NEAR			1.0f		//どこまで近くを写すか
+#define CAMERA_FAR			60000.0f	//どこまで遠くを写すか
+#define CAMERA_HEIGHT		300.0f		//カメラの注視点の高さ
+#define CAMERA_DISTANCE		10.0f		//カメラと注視点の距離
+#define CAMERA_INIT_V_ANGLE	0.0f		//カメラの向きデフォルト(垂直) / -90が上 90が下
+#define CAMERA_INIT_H_ANGLE	0.0f		//カメラの向きデフォルト(水平) / -90が左 90が右
+#define CAMERA_ANGLE_X_PLUS	1.0f		//カメラの角度(X)の加算値
+#define CAMERA_ANGLE_MAX_X	90.0f		//カメラのMAX角度(X)
+#define CAMERA_ANGLE_MAX_Y	90.0f		//カメラのMAX角度(Y)
+
+//マップ
+#define MAP_TATE_MAX	150
+#define MAP_YOKO_MAX	150
+
+#define MAP_TATE_SIZE	(100.0f * 2.0f) //間隔を2.0倍開ける
+#define MAP_YOKO_SIZE	(100.0f * 2.0f)
+
+//マグロ関係
+#define MAGURO_MOVE			10.0f	//スピード
+#define MAGURO_CAP_RADIUS	30.0f	//当たり判定のカプセルの半径
+
+//エラーメッセージ
+#define ERR_LOAD_TITLE_IMAGE	TEXT("画像読み込みエラー")
+#define ERR_LOAD_TITLE_MODEL	TEXT("モデル読み込みエラー")
+
+//終了ダイアログ用
+#define CLOSE_WINDOW_TITLE		TEXT("ゲーム終了")
+#define CLOSE_WINDOW_CAPTION	TEXT("ゲームを終了しますか？")
+
+//ゲームのシーン
+enum GAME_SCENE
+{
+	GAME_SCENE_START,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END
+};
+
+//int型のPOINT構造体
+typedef struct STRUCT_I_POINT
+{
+	//座標を初期化
+	int x = -1;
+	int y = -1;
+
+}iPOINT;
+
+//画像構造体
+typedef struct STRUCT_IMAGE
+{
+	char path[PATH_MAX] = { '\0' };	//パス
+	int handle = -1;				//ハンドル
+	int x = 0;						//X位置
+	int y = 0;						//Y位置
+	int width = 0;					//幅
+	int height = 0;					//高さ
+	BOOL IsDraw = FALSE;			//描画できるか(デフォルトはFALSE)
+
+}IMAGE;
+
+//3Dモデル構造体
+typedef struct STRUCT_MODEL
+{
+	char path[PATH_MAX] = { '\0' };		//パス
+	int handle = -1;					//ハンドル
+	VECTOR pos = { 0.0f,0.0f,0.0f };	//モデルの位置
+	VECTOR move = { 0.0f,0.0f,0.0f };	//モデルの動作
+	float angle = 0.0f;		//モデルの回転
+	BOOL IsDraw = FALSE;	//描画できるか(デフォルトはFALSE)
+	double VecSin = 0.0f;	//プレイヤーの向いている向き：弧度(sin = Y)
+	double VecCos = 0.0f;	//プレイヤーの向いている向き：弧度(cos = X)
+	float HAngle = 0.0f;	//水平角度
+	BOOL IsMove = TRUE;		//動けるか
+
+}MODEL;
+
+//カメラ構造体
+typedef struct STRUCT_CAMERA
+{
+	float CamNear = 0.0f;				//どこまで近くを写すか
+	float CamFar = 0.0f;				//どこまで遠くを写すか
+	float CamHeight = 0.0f;				//カメラの高さの位置
+	float VAngle = 0.0;					//カメラの垂直の角度
+	float HAngle = 0.0;					//カメラの水平の角度
+	VECTOR pos = { 0.0f,0.0f,0.0f };	//カメラの場所
+	float angle = 90.0f;				//カメラの角度
+
+}CAMERA;
 
 //########## グローバル変数 ##########
 int ModelHandle;						//3Dモデルのハンドルを入れる
@@ -45,7 +154,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	//３Ｄモデルの読み込み
 	//ModelHandle = MV1LoadModel("cube.mv1");
-	ModelHandle = MV1LoadModel("mqoモデル\\マグロ\\マグロ.mqo");
+	ModelHandle = MV1LoadModel(".\\mqoモデル\\マグロ\\マグロ.mqo");
 
 
 	//画面に映る位置に３Ｄモデルを移動
